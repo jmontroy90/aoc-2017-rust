9b48f96c69ba2bc93bfaaae07ff2c05fdc7c0b33
John Montroy <jmontroy90@gmail.com>
2025-05-23 15:13:26 -0400

    refactor: duplicated code

--- src/day1/day1.rs
+++ src/day1/day1.rs
@@ -1,30 +1,17 @@
 use std::fs;
+use std::io;
 
 pub fn run_day1(input_file: &str) {
     let input = load_input(input_file);
     if let Ok(ins) = input {
         println!("{}", part1(&ins));
-        println!("{}", part2(&ins));    
+        println!("{}", part2(&ins));
     }
-    
 }
-
-fn part1(input: &[u32]) -> u32 {
+fn sum_equal_elems(input: &[u32], offset: usize) -> u32 {
     let mut run_sum = 0;
     for (i, c) in input.iter().enumerate() {
-        let next_ix = (i + 1) % input.len();
-        if *c == input[next_ix] {
-            run_sum += c;
-        }
-    }
-    run_sum
-}
-
-fn part2(input: &[u32]) -> u32 {
-    let mut run_sum = 0;
-    let hop = input.len() / 2;
-    for (i, c) in input.iter().enumerate() {
-        let cmp_ix = (i + hop) % input.len();
+        let cmp_ix = (i + offset) % input.len();
         if *c == input[cmp_ix] {
             run_sum += c
         }
@@ -32,7 +19,15 @@ fn part2(input: &[u32]) -> u32 {
     run_sum
 }
 
-fn load_input(file_name: &str) -> Result<Vec<u32>, std::io::Error> {
+fn part1(input: &[u32]) -> u32 {
+    sum_equal_elems(input, 1)
+}
+
+fn part2(input: &[u32]) -> u32 {
+    sum_equal_elems(input, input.len() / 2)
+}
+
+fn load_input(file_name: &str) -> Result<Vec<u32>, io::Error> {
     let content = fs::read_to_string(file_name)?;
     let digits: Vec<u32> = content.chars().flat_map(|c| c.to_digit(10)).collect();
     Ok(digits)
