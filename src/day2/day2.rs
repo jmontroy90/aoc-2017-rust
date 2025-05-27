6d41088cab9cf31c7a6e6d12682c8b978bdbd0fc
John Montroy <jmontroy90@gmail.com>
2025-05-27 17:12:49 -0400

    refactor: extract per-line logic into separate functions. test: add table tests from site examples.

--- src/day2/day2.rs
+++ src/day2/day2.rs
@@ -13,20 +13,25 @@ pub fn run_day2(input_file: &str) {
 fn part1(input: Vec<Vec<u32>>) -> u32 {
     let mut sum = 0;
     for line in input {
-        let (min, max) = line.iter().fold((u32::MAX, 0), |(min, max), &i| (min.min(i), max.max(i)));
-        sum += max - min;
-    };
-    sum
-}
-fn part2(input: Vec<Vec<u32>>) -> u32 {
-    let mut sum = 0;
-    for line in input {
-        sum += find_divisible_sum(line)
+        sum += find_minmax_diff(&line);
     };
     sum
 }
 
-fn find_divisible_sum(ns: Vec<u32>) -> u32 {
+fn find_minmax_diff(ns: &[u32]) -> u32 {
+    let (min, max) = ns.iter().fold((u32::MAX, 0), |(min, max), &i| (min.min(i), max.max(i)));
+    max - min
+}
+
+fn part2(input: Vec<Vec<u32>>) -> u32 {
+    let mut sum = 0;
+    for line in input {
+        sum += find_divisible_sum(&line)
+    };
+    sum
+}
+
+fn find_divisible_sum(ns: &[u32]) -> u32 {
     for (i, n1) in ns.iter().enumerate() {
         for n2 in ns[i+1..].iter() {
             if n1 % n2 == 0 {
@@ -50,42 +55,27 @@ fn load_input(file_name: &str) -> Result<Vec<Vec<u32>>, io::Error> {
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn test_part1() {
-    //     let tests: Vec<(&str, u32)> = vec![
-    //         ("1122", 3),
-    //         ("1234", 0),
-    //         ("1111", 4),
-    //         ("91212129", 9)
-    //     ];
-    //     for (input, expected) in tests {
-    //         assert_eq!(part1(&input.to_digits()), expected)    
-    //     }
-    // }
-    // 
-    // #[test]
-    // fn test_part2() {
-    //     let tests: Vec<(&str, u32)> = vec![
-    //         ("1212", 6),
-    //         ("1221", 0),
-    //         ("123425", 4),
-    //         ("123123", 12),
-    //         ("12131415", 4),
-    //     ];
-    //     for (input, expected) in tests {
-    //         assert_eq!(part2(&input.to_digits()), expected);
-    //     }
-    // }
+    #[test]
+    fn test_find_minmax_diff() {
+        let tests: Vec<(&[u32], u32)> = vec![
+            (&[5, 1, 9, 5], 8),
+            (&[7, 5, 3], 4),
+            (&[2, 4, 6, 8], 6),
+        ];
+        for (input, expected) in tests {
+            assert_eq!(find_minmax_diff(input), expected)
+        }
+    }
 
-
+    #[test]
+    fn test_part2() {
+        let tests: Vec<(&[u32], u32)> = vec![
+            (&[5, 9, 2, 8], 4),
+            (&[9, 4, 7, 3], 3),
+            (&[3, 8, 6, 5], 2)
+        ];
+        for (input, expected) in tests {
+            assert_eq!(find_divisible_sum(input), expected);
+        }
+    }
 }
-
-// trait ToDigits {
-//     fn to_digits(&self) -> Vec<u32>;
-// }
-// 
-// impl ToDigits for &str {
-//     fn to_digits(&self) -> Vec<u32> {
-//         self.chars().filter_map(|c| c.to_digit(10)).collect()
-//     }
-// }
